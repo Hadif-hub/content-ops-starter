9b1efc3f2e7ee4dedcdcd9d428f7bce95981b182
Hadif-hub <denmasaklomak@gmail.com>
2024-12-05 05:28:21 +0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,4 @@
-import Head from 'next/head';
 import '../css/main.css';
-
-export function Home() {
-  return (
-    <>
-      <Head>
-        <title>My Awesome Website</title>
-      </Head>
-      <h1>Welcome to My Cool Site!</h1>
-    </>
-  );
-}
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
